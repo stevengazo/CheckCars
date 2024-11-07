fc9d25808312339f0353bf1dcec2d82d9e7813b8
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2024-11-06 21:19:01 -0600

    feat : data

---
+++ CheckCars/ViewModels/ViewEntryExitVM.cs
@@ -0,0 +1,53 @@
+﻿using CheckCars.Data;
+using CheckCars.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckCars.ViewModels
+{
+    public class ViewEntryExitVM : INotifyPropertyChangedAbst
+    {
+        #region General
+        // Implementación de INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Método para notificar cambios en las propiedades
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+        private EntryExitReport _Report = new();
+
+        public EntryExitReport Report
+        {
+            get { return _Report; }
+            set
+            {
+                if (_Report != value)
+                {
+                    _Report = value;
+                    OnPropertyChanged(nameof(Report));
+                }
+            }
+        }
+
+
+        public ViewEntryExitVM()
+        {
+            var Id = Data.StaticData.ReportId;
+
+            using (var dbo = new ReportsDBContextSQLite())
+            {
+                Report = dbo.EntryExitReports.Include(E=>E.Photos).FirstOrDefault(e => e.ReportId == Id);
+            }
+        }
+    }
+}
